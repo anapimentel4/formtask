ba6cfff5e5255737a43d49d8e4e885328c27d7bb
Ana Pimenetl <anapimentel004@gmail.com>
2023-05-22 08:46:42 -0400

    modificacion del formulario js eventos y validaciones agregados a cada campo de los inpts

--- js/registro.js
+++ js/registro.js
@@ -20,23 +20,33 @@ inputPassword .addEventListener('blur', validar);
 inputSubmit   .addEventListener('blur', validar);
 
 function validar(e){
+//console.log(e.target.parentElement.nextElemtSibling) rrecorrer el DOM
     if(e.target.value.trim() === ''){
-      mostarAlerta(`el campo ${e.target.id} es obligatorio `)
-    }else {
-        console.log('escribio algo  ')
+      mostarAlerta(`el campo ${e.target.id} es obligatorio` , e.target.parentElement)
+      return;
     }
+    limpiarAlerta(e.target.parentElement);
 }
  
-function mostarAlerta(mensaje){
+function mostarAlerta(mensaje, referencia){
+  limpiarAlerta(referencia);
+
     const error = document.createElement('P');
     error.textContent = mensaje; // TextContent add text to an element or get  text  from element , can use inner too but..... 
-    error.classList.add('bg-red-600', 'text-white', 'p-0', 'text-center')
+    error.classList.add('bg-red-600', 'text-white', 'p-1', )
 
 
 
     //push the error to the Form 
 
-    formulario.appendChild(error) // add a new element to the existed one 
+    referencia.appendChild(error) // add a new element to the existed one 
 }
+ function limpiarAlerta(referencia){
+    // comprobar si ya existe una alerta
+    const alerta = referencia.querySelector('.bg-red-600');
+    if(alerta){
+        alerta.remove();
+    }
+ }
 
 })
